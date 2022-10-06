e19337628ab2e2829d826c1775ad0899c849b4bf
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-06 23:17:29 +0200

    Final

--- app/src/main/java/com/example/intentimplicito/MainActivity.java
+++ app/src/main/java/com/example/intentimplicito/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.intentimplicito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -18,10 +20,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(binding.getRoot());
 
         binding.btAgregar.setOnClickListener(this);
+        binding.btLlamar112.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:112"));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
-
+        Intent intent = new Intent(this, SegundoActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/intentimplicito/SegundoActivity.java
+++ app/src/main/java/com/example/intentimplicito/SegundoActivity.java
@@ -2,7 +2,10 @@ package com.example.intentimplicito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
 
 import com.example.intentimplicito.databinding.ActivitySegundoBinding;
 
@@ -15,5 +18,18 @@ public class SegundoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding2 = ActivitySegundoBinding.inflate(getLayoutInflater());
         setContentView(binding2.getRoot());
+    }
+
+    public void crearContacto(View view) {
+        String nombre = binding2.txtNombre.getText().toString();
+        String numTelefono = binding2.txtTelefono.getText().toString();
+        String email = binding2.txtEmail.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_INSERT);
+        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+        intent.putExtra(ContactsContract.Intents.Insert.NAME, nombre);
+        intent.putExtra(ContactsContract.Intents.Insert.PHONE, numTelefono);
+        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, email);
+        startActivity(intent);
     }
 }
